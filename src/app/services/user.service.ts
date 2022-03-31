ae527ddbb880381308b1eb313574e2e8997a1b0b
Hichoow <h.elfaquir52@gmail.com>
2022-03-31 22:34:03 +0200

    Changed Frontend

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url: string = "http://localhost:8080/api/auth"
+  private httpOptions = {
+    headers: new HttpHeaders().set("Content-Type", "application/json")
+  };
+
+  constructor(private http: HttpClient) { }
+
+  public login(username: string, password: string): Observable<any>{
+    let body = {
+      "username" : username,
+      "password" : password
+    }
+
+    return this.http.post(this.url + "/login", body, this.httpOptions);
+  }
+
+  public register(username: string,email: string, password: string): Observable<any>{
+    let body = {
+      "username" : username,
+      "email" : email,
+      "password" : password
+    }
+
+    return this.http.post(this.url + "/register", body, this.httpOptions);
+  }
+}
